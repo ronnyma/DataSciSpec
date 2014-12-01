23c74450f828f3b2530b5da50cfbfe71cf154614
ronnyma <ronnyma@transfinite.no>
2014-12-01 19:51:02 +0100

    Strip away NA's

--- R-prog/ProgrammingAssignment1/corr.R
+++ R-prog/ProgrammingAssignment1/corr.R
@@ -4,7 +4,8 @@
   
 corr <- function(directory, threshold = 0) {
   # Set wd
-  setwd( "/home/ronnyma/development/DataSciSpec/R-prog/Assignment1" )
+  setwd("/usr/home/ronnyma/development/DataSciSpec/R-prog/ProgrammingAssignment1")
+
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   files <- Sys.glob(paste(directory,"*", sep="/"))
@@ -16,13 +17,18 @@ corr <- function(directory, threshold = 0) {
     tmp <- read.csv( f, header=TRUE )
     cc <- sum( complete.cases( tmp ) )
     if( cc > threshold ) {
-      print(sprintf( "Adding %s, threshold < %d", f, cc ) )
+      #print(sprintf( "Adding %s, threshold < %d", f, cc ) )
       alldata <- rbind( tmp, alldata )
     }
     else {
-      print(sprintf( "Not adding %s, threshold > %d", f, cc ) )
+      #print(sprintf( "Not adding %s, threshold > %d", f, cc ) )
       #alldata <- rbind( c(), alldata )
     }
+  }
+  if( length( alldata ) == 0) {
+    i <- c()
+    return( i )
+     
   }
   # Strip away the NAs
   all_strip <- alldata[complete.cases(alldata),]
